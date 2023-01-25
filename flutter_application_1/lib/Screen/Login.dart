4bce345392a32f940fa50751baad91662c8250e8
Manohar choudhary <73361258+manoharchoudhary@users.noreply.github.com>
2023-01-25 12:46:30 +0530

    created  signup and welcome

--- flutter_application_1/lib/Screen/Login.dart
+++ flutter_application_1/lib/Screen/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screen/Signup.dart';
+import 'package:flutter_application_1/Screen/Success.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatelessWidget {
@@ -7,19 +9,19 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 213, 242, 232),
+        backgroundColor: const Color.fromARGB(255, 213, 242, 232),
         body: Center(
             child: Column(
           children: [
             Container(
               width: 339,
               height: 58,
-              margin: EdgeInsets.fromLTRB(0, 90, 0, 19),
+              margin: const EdgeInsets.fromLTRB(0, 90, 0, 19),
               child: Text(
                 "Welcome back!",
                 style: GoogleFonts.inter(
                   fontSize: 24,
-                  color: Color.fromARGB(255, 0, 0, 0),
+                  color: const Color.fromARGB(255, 0, 0, 0),
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -27,18 +29,18 @@ class Login extends StatelessWidget {
             Container(
               width: 339,
               height: 58,
-              margin: EdgeInsets.only(top: 67),
+              margin: const EdgeInsets.only(top: 17),
               child: Text(
                 "Log In to your account",
                 style: GoogleFonts.inter(
-                    fontSize: 18, color: Color.fromARGB(198, 0, 0, 0)),
+                    fontSize: 18, color: const Color.fromARGB(198, 0, 0, 0)),
                 textAlign: TextAlign.left,
               ),
             ),
             Container(
                 width: 340,
                 height: 55,
-                margin: EdgeInsets.only(bottom: 47),
+                margin: const EdgeInsets.only(bottom: 47),
                 child: const TextField(
                   decoration: InputDecoration(
                       border: OutlineInputBorder(),
@@ -46,10 +48,12 @@ class Login extends StatelessWidget {
                       filled: true,
                       fillColor: Colors.white),
                 )),
-            Container(
+            // ignore: prefer_const_constructors
+            SizedBox(
                 width: 340,
                 height: 55,
                 child: const TextField(
+                  obscureText: true,
                   decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: 'Enter your password',
@@ -61,19 +65,24 @@ class Login extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Remember Me",
-                    style: GoogleFonts.inter(
-                      fontSize: 14,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    ),
-                    textAlign: TextAlign.center,
+                  Row(
+                    children: [
+                      Icon(Icons.check_box_outlined),
+                      Text(
+                        "Remember Me",
+                        style: GoogleFonts.inter(
+                          fontSize: 14,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
                   Text(
-                    "Forgot Password",
+                    "Forgotten password?",
                     style: GoogleFonts.inter(
                       fontSize: 14,
-                      color: Color.fromARGB(255, 0, 0, 0),
+                      color: const Color.fromARGB(255, 0, 0, 0),
                     ),
                     textAlign: TextAlign.center,
                   ),
@@ -84,7 +93,7 @@ class Login extends StatelessWidget {
               // ignore: sort_child_properties_last
               child: ElevatedButton(
                 style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all(const Size(340, 55)),
+                    minimumSize: MaterialStateProperty.all(const Size(196, 55)),
                     backgroundColor: MaterialStateProperty.all(
                         const Color.fromARGB(255, 45, 73, 144))),
                 child: Text(
@@ -93,22 +102,81 @@ class Login extends StatelessWidget {
                       color: const Color.fromARGB(255, 250, 250, 250),
                       fontSize: 24),
                 ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const Success()));
+                },
+              ),
+              margin: const EdgeInsets.only(bottom: 25, top: 30),
+            ),
+            Text(
+              "Or",
+              style: GoogleFonts.inter(
+                fontSize: 14,
+                color: const Color.fromARGB(255, 0, 0, 0),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              width: 340,
+              height: 55,
+              margin: const EdgeInsets.only(bottom: 40, top: 25),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    minimumSize: MaterialStateProperty.all(const Size(340, 55)),
+                    side: MaterialStateProperty.all(const BorderSide(
+                        width: 1.0,
+                        color: Color.fromARGB(255, 246, 246, 246)))),
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.android,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'Log in with your Google account',
+                      style: TextStyle(
+                          fontSize: 18.0, color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                  ],
+                ),
                 onPressed: () {},
               ),
-              margin: EdgeInsets.only(bottom: 19),
             ),
-            OutlinedButton(
-              style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all(const Size(340, 55)),
-                  side: MaterialStateProperty.all(const BorderSide(
-                      width: 1.0, color: Color.fromARGB(255, 45, 73, 144)))),
-              child: const Text(
-                'Signup',
-                style: TextStyle(
-                    fontSize: 20.0, color: Color.fromARGB(255, 45, 73, 144)),
+            Container(
+              width: 220,
+              alignment: Alignment.center,
+              child: Row(
+                children: [
+                  Text(
+                    "Dont’t have an account? ",
+                    style: GoogleFonts.inter(
+                      fontSize: 14,
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  GestureDetector(
+                    child: Text(
+                      "Sign Up",
+                      style: GoogleFonts.inter(
+                          fontSize: 14,
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const Signin()));
+                    },
+                  ),
+                ],
               ),
-              onPressed: () {},
-            )
+            ),
           ],
         )));
   }
---
+++ flutter_application_1/lib/Screen/Success.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screen/Home.dart';
+import 'package:flutter_application_1/Screen/Login.dart';
+import 'package:flutter_application_1/Screen/Signup.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Success extends StatelessWidget {
+  const Success({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 220),
+          child: const Icon(
+            Icons.check,
+            size: 150.0,
+            color: Color.fromRGBO(45, 73, 144, 1),
+          ),
+        ),
+        Container(
+          width: 339,
+          margin: EdgeInsets.only(top: 50),
+          height: 58,
+          child: Text(
+            "Your Sign up was successful",
+            style: GoogleFonts.inter(
+                fontSize: 16, color: Color.fromARGB(198, 0, 0, 0)),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        GestureDetector(
+          child: Text(
+            "Continue to Home",
+            style: GoogleFonts.inter(
+                fontSize: 14,
+                color: Colors.blue,
+                decoration: TextDecoration.underline),
+            textAlign: TextAlign.center,
+          ),
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => const Home()));
+          },
+        ),
+      ],
+    )));
+  }
+}
